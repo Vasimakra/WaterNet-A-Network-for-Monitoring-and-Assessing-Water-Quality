fe4768c19320382ebc7aee9fad88013afbcc7a5b
Vasimakra <120660357+Vasimakra@users.noreply.github.com>
2023-07-30 13:29:33 +0530

    Files Upload

---
+++ WaterNet/run.bat
@@ -0,0 +1,29 @@
+pip install xlwt
+
+pip install openpyxl
+
+pip install sklearn
+
+pip install pandas
+
+pip install matplotlib
+
+pip install nltk
+
+pip install wordcloud
+
+pip install numpy
+
+pip install seaborn
+
+pip install scikit-learn==0.22.2.post1
+
+pip install sklearn-extensions==0.0.2
+
+pip install pyswarms==1.1.0
+
+pip install django==1.11.5
+
+pip install mysqlclient==1.3.12
+
+pause
